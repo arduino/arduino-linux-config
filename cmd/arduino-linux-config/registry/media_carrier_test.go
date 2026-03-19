6bed1230b9a5c9d325778a0452107d28edd014c3
Marta Carbone <m.carbone@arduino.cc>
2026-03-19 08:31:23 +0100

    Test input validation.

--- cmd/arduino-linux-config/registry/media_carrier_test.go
+++ cmd/arduino-linux-config/registry/media_carrier_test.go
@@ -2,6 +2,8 @@ package registry
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestIsOptionValid(t *testing.T) {
@@ -35,6 +37,57 @@ func TestIsOptionValid(t *testing.T) {
 				t.Errorf("IsOptionValid(%v, %v) = %v; want %v",
 					tt.deviceName, tt.option, got, tt.want)
 			}
+		})
+	}
+}
+
+func TestValidateInput(t *testing.T) {
+	tests := []struct {
+		name       string
+		rawDevice  string
+		rawOption  string
+		wantDevice MediaCarrierDeviceName
+		wantErr    bool
+	}{
+		{
+			name:       "Valid device and valid option",
+			rawDevice:  "camera1",
+			rawOption:  "type1-2lane",
+			wantDevice: Camera1,
+			wantErr:    false,
+		},
+		{
+			name:       "Unknown device",
+			rawDevice:  "toaster",
+			rawOption:  "burn",
+			wantDevice: "",
+			wantErr:    true,
+		},
+		{
+			name:       "Known device but invalid option",
+			rawDevice:  "camera1",
+			rawOption:  "invalid-option",
+			wantDevice: "",
+			wantErr:    true,
+		},
+		{
+			name:       "Known device none option",
+			rawDevice:  "camera1",
+			rawOption:  "none",
+			wantDevice: Camera1,
+			wantErr:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotDevice, err := ValidateInput(tt.rawDevice, tt.rawOption)
+			if tt.wantErr {
+				require.NotNil(t, err)
+			} else {
+				require.Nil(t, err)
+			}
+			require.Equal(t, tt.wantDevice, gotDevice)
 		})
 	}
 }
