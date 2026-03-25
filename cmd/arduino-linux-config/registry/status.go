6679c96a16797c097e05229c4b39d0b2752a8a31
Marta Carbone <m.carbone@arduino.cc>
2026-03-25 07:35:01 +0100

    Address status file permission to be readable by all users. This will avoid to ask for a password to show the status

--- cmd/arduino-linux-config/registry/status.go
+++ cmd/arduino-linux-config/registry/status.go
@@ -146,6 +146,7 @@ func saveStatusFile(statusFile *paths.Path, status StatusFile) error {
 		return fmt.Errorf("marshal error: %w", err)
 	}
 
+	// nolint:gosec // G306: Status file must be readable
 	err = os.WriteFile(statusFile.String(), data, 0644)
 	if err != nil {
 		return fmt.Errorf("write error: %w", err)
