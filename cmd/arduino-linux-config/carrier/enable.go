7fb7753510561695c458ba4037ebfd7ddc813348
Marta Carbone <m.carbone@arduino.cc>
2026-04-18 18:09:51 +0200

    Add media-carrier configuration example.

--- cmd/arduino-linux-config/carrier/enable.go
+++ cmd/arduino-linux-config/carrier/enable.go
@@ -17,7 +17,9 @@ func newEnableCmd(cfg config.Configuration) *cobra.Command {
 	return &cobra.Command{
 		Use:   "enable <carrier-name> [device=option...]",
 		Short: "Enable and configure a carrier with the specified device options",
-		Args:  cobra.MinimumNArgs(1),
+		Example: `  # To configure a media-carrier with two cameras attached, one type1 and one type2:
+  arduino-linux-config carrier enable media-carrier camera0=type1-2lane camera1=type2-4lane`,
+		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			carrierName := args[0]
 			deviceOptions := args[1:]
