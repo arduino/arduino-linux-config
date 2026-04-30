492fe5f8df27279d344c95c6a33e543f07b47782
mirkoCrobu <m.crobu@ext.arduino.cc>
2026-04-30 12:22:17 +0200

    add integration test

---
+++ tests/integration/docker_helper_test.go
@@ -0,0 +1,79 @@
+//go:build integration
+
+package integration
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	dockerImageName = "arduino-linux-config-test"
+	containerName   = "arduino-linux-config-test-container"
+	arch            = "arm64"
+)
+
+func buildDockerImage(t testing.TB) {
+	if t != nil {
+		t.Helper()
+	}
+	root := repoRoot()
+	cmd := exec.Command("docker", "build",
+		"--platform", "linux/"+arch,
+		"--build-arg", "ARCH="+arch,
+		"-t", dockerImageName,
+		"-f", filepath.Join(root, "tests/integration/Dockerfile"),
+		root,
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		if t != nil {
+			t.Fatalf("failed to build docker image: %v", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "failed to build docker image: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func repoRoot() string {
+	out, err := exec.Command("git", "rev-parse", "--show-toplevel").Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get repo root: %v\n", err)
+		os.Exit(1)
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func startDockerContainer(t *testing.T) {
+	t.Helper()
+	cmd := exec.Command("docker", "run", "-d", "--name", containerName, dockerImageName, "sleep", "infinity")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	require.NoError(t, err, "failed to start docker container")
+}
+
+func stopDockerContainer(t *testing.T) {
+	t.Helper()
+	cmd := exec.Command("docker", "rm", "-f", containerName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	_ = cmd.Run()
+}
+
+func execInContainer(t *testing.T, args ...string) string {
+	t.Helper()
+	dockerArgs := append([]string{"exec", containerName}, args...)
+	cmd := exec.Command("docker", dockerArgs...)
+	out, err := cmd.CombinedOutput()
+	require.NoError(t, err, "failed to exec in container: %v\noutput: %s", args, string(out))
+	return string(out)
+}
---
+++ tests/integration/integration_test.go
@@ -0,0 +1,180 @@
+//go:build integration
+
+package integration
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type deviceResult struct {
+	Device     string `json:"device"`
+	Option     string `json:"option"`
+	DeviceType string `json:"device_type"`
+}
+
+type carrierResult struct {
+	CarrierName    string         `json:"carrier_name"`
+	CurrentEnabled bool           `json:"current_enabled"`
+	NextEnabled    bool           `json:"next_enabled"`
+	Current        []deviceResult `json:"current"`
+	Next           []deviceResult `json:"next"`
+	Warnings       []string       `json:"warnings"`
+}
+
+type showResult struct {
+	Carriers []carrierResult `json:"carriers"`
+}
+
+func TestMain(m *testing.M) {
+	buildDockerImage(nil)
+	os.Exit(m.Run())
+}
+
+func TestCarrierShow_Empty(t *testing.T) {
+	startDockerContainer(t)
+	t.Cleanup(func() { stopDockerContainer(t) })
+
+	out := execInContainer(t, "arduino-linux-config", "carrier", "show", "--format", "json")
+
+	var result showResult
+	err := json.Unmarshal([]byte(out), &result)
+	require.NoError(t, err, "output should be valid JSON")
+
+	require.Len(t, result.Carriers, 1)
+
+	carrier := result.Carriers[0]
+	require.Equal(t, "media-carrier", carrier.CarrierName)
+	require.Equal(t, false, carrier.CurrentEnabled)
+	require.Equal(t, false, carrier.NextEnabled)
+
+	expectedDevices := []deviceResult{
+		{Device: "camera0", Option: "none", DeviceType: "camera"},
+		{Device: "camera1", Option: "none", DeviceType: "camera"},
+		{Device: "display", Option: "none", DeviceType: "display"},
+	}
+
+	require.Equal(t, expectedDevices, carrier.Current)
+	require.Equal(t, expectedDevices, carrier.Next)
+}
+
+func TestCarrierEnable_AllDevices(t *testing.T) {
+	startDockerContainer(t)
+	t.Cleanup(func() { stopDockerContainer(t) })
+
+	out := execInContainer(t, "arduino-linux-config", "carrier", "enable", "media-carrier",
+		"camera0=type1-2lanes",
+		"camera1=type1-2lanes",
+		"display=8-dsi-touch-a",
+		"--format", "json",
+	)
+	fmt.Println(out)
+	var result carrierResult
+	err := json.Unmarshal([]byte(out), &result)
+	require.NoError(t, err, "output should be valid JSON")
+
+	require.Equal(t, "media-carrier", result.CarrierName)
+	require.Equal(t, false, result.CurrentEnabled)
+	require.Equal(t, true, result.NextEnabled)
+
+	expectedCurrent := []deviceResult{
+		{Device: "camera0", Option: "none", DeviceType: "camera"},
+		{Device: "camera1", Option: "none", DeviceType: "camera"},
+		{Device: "display", Option: "none", DeviceType: "display"},
+	}
+
+	expectedNext := []deviceResult{
+		{Device: "camera0", Option: "type1-2lanes", DeviceType: "camera"},
+		{Device: "camera1", Option: "type1-2lanes", DeviceType: "camera"},
+		{Device: "display", Option: "8-dsi-touch-a", DeviceType: "display"},
+	}
+
+	require.Equal(t, expectedCurrent, result.Current)
+	require.Equal(t, expectedNext, result.Next)
+}
+
+func TestCarrierDisable(t *testing.T) {
+	startDockerContainer(t)
+	t.Cleanup(func() { stopDockerContainer(t) })
+
+	// First enable all devices
+	execInContainer(t, "arduino-linux-config", "carrier", "enable", "media-carrier",
+		"camera0=type1-2lanes",
+		"camera1=type1-2lanes",
+		"display=8-dsi-touch-a",
+	)
+
+	// Then disable
+	out := execInContainer(t, "arduino-linux-config", "carrier", "disable", "media-carrier", "--format", "json")
+
+	var result carrierResult
+	err := json.Unmarshal([]byte(out), &result)
+	require.NoError(t, err, "output should be valid JSON")
+
+	require.Equal(t, "media-carrier", result.CarrierName)
+	require.Equal(t, false, result.CurrentEnabled)
+	require.Equal(t, false, result.NextEnabled)
+
+	expectedDevices := []deviceResult{
+		{Device: "camera0", Option: "none", DeviceType: "camera"},
+		{Device: "camera1", Option: "none", DeviceType: "camera"},
+		{Device: "display", Option: "none", DeviceType: "display"},
+	}
+
+	require.Equal(t, expectedDevices, result.Current)
+	require.Equal(t, expectedDevices, result.Next)
+}
+
+type listDeviceResult struct {
+	Name             string   `json:"name"`
+	DeviceType       string   `json:"device_type"`
+	AvailableDevices []string `json:"available_devices"`
+}
+
+type listCarrierResult struct {
+	Name    string             `json:"name"`
+	Devices []listDeviceResult `json:"devices"`
+}
+
+type listResult struct {
+	Carriers []listCarrierResult `json:"carriers"`
+}
+
+func TestCarrierList(t *testing.T) {
+	startDockerContainer(t)
+	t.Cleanup(func() { stopDockerContainer(t) })
+
+	out := execInContainer(t, "arduino-linux-config", "carrier", "list", "--format", "json")
+
+	var result listResult
+	err := json.Unmarshal([]byte(out), &result)
+	require.NoError(t, err, "output should be valid JSON")
+
+	require.Len(t, result.Carriers, 1)
+
+	carrier := result.Carriers[0]
+	require.Equal(t, "media-carrier", carrier.Name)
+	require.Len(t, carrier.Devices, 3)
+
+	require.Equal(t, listDeviceResult{
+		Name:             "camera0",
+		DeviceType:       "camera",
+		AvailableDevices: []string{"none", "type1-2lanes", "type1-4lanes"},
+	}, carrier.Devices[0])
+
+	require.Equal(t, listDeviceResult{
+		Name:             "camera1",
+		DeviceType:       "camera",
+		AvailableDevices: []string{"none", "type1-2lanes", "type1-4lanes"},
+	}, carrier.Devices[1])
+
+	require.Equal(t, listDeviceResult{
+		Name:             "display",
+		DeviceType:       "display",
+		AvailableDevices: []string{"none", "8-dsi-touch-a"},
+	}, carrier.Devices[2])
+}
