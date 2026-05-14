8ca750795d2d16c3965572abbdd9ceeebef621e3
mirkoCrobu <m.crobu@ext.arduino.cc>
2026-05-14 14:02:17 +0200

    fmt lint

--- internal/sync/linux_sync.go
+++ internal/sync/linux_sync.go
@@ -1,9 +1,10 @@
-//go:build linux
-
 // This file is part of arduino-linux-config.
 //
 // SPDX-FileCopyrightText: Arduino s.r.l. and/or its affiliated companies
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+//go:build linux
+
 package sync
 
 import "golang.org/x/sys/unix"
--- internal/sync/windows_sync.go
+++ internal/sync/windows_sync.go
@@ -1,11 +1,11 @@
-//go:build windows
-
 // This file is part of arduino-linux-config.
 //
 // SPDX-FileCopyrightText: Arduino s.r.l. and/or its affiliated companies
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-package package sync
+//go:build windows
+
+package sync
 
 func SyncToDisk() {
 	// No-op on Windows, as the OS handles disk synchronization automatically.
